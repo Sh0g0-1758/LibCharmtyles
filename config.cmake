2c03c22c7439602c8833ce89231fa716205ce996
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-09-27 11:11:00 +0530

    Revert "cmake build switch"
    This reverts commit 1f82f03aced6c88d41434c74489056675f2ddc87.

--- config.cmake
+++ config.cmake
@@ -3,7 +3,7 @@ set(BASE_DIR "/u/ajain18/LibCharmtyles")
 set(EIGEN_DIR "/sw/spack/deltas11-2023-03/apps/linux-rhel8-zen3/gcc-11.4.0/eigen-3.4.0-vqm46bt")
 
 set(CHARMC "${CHARM_DIR}/bin/charmc")
-set(OPTS "-std=c++17 -O3 -march=native -DNDEBUG")
+set(OPTS "-c++-option -std=c++17 -O3 -march=native -DNDEBUG")
 set(LD_OPTS "")
 set(INCS "-I${BASE_DIR}")
 
